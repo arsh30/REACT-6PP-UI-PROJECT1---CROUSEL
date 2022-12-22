a338c8cd4cdcbee009757ceee3b623040bd42e1b
Arsh <as3096734@gmail.com>
2022-12-23 05:17:35 +0530

    Project 1 finish - UI REACT

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow us</h5>
+        <div>
+          <a href="https://youtube.come" target={"_blank"}>
+            Youtube
+          </a>
+          <a href="https://instagram.come" target={"_blank"}>
+            Instagram
+          </a>
+          <a href="https://github.come" target={"_blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+
+const Header = () => {
+  return (
+    <nav>
+      <h1>TechStar</h1>
+      <main>
+        <HashLink to="/#home">Home</HashLink>
+        <HashLink to="/#about">About</HashLink>
+        {/* #about -> coz hum ik div bnaege jisko id dege about to scroll hokr udr 
+        aajege , isliye css me scroll behaviour smooth kra*/}
+        <HashLink to="/contact">Contact</HashLink>
+        <HashLink to="/#brands">Brands</HashLink>
+        <HashLink to="/services">Services</HashLink>
+      </main>
+    </nav>
+  );
+};
+
+export default Header;
